b22d054de33ad48dbb3bdf49f297d0fd7a9f6288
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-03-01 14:58:08 -0700

    fixing my default returns.........

--- Labs/Lab3/fractions.cpp
+++ Labs/Lab3/fractions.cpp
@@ -6,22 +6,27 @@ fractions::Fraction::Fraction(int numerator, int denominator)
 
 fractions::Fraction fractions::Fraction::operator+(fractions::Fraction const &frac)
 {
+    return Fraction(0, 1);
 }
 
 fractions::Fraction fractions::Fraction::operator-(fractions::Fraction const &frac)
 {
+    return Fraction(0, 1);
 }
 
 fractions::Fraction fractions::Fraction::operator*(Fraction const &frac)
 {
+    return Fraction(0, 1);
 }
 
 fractions::Fraction fractions::Fraction::operator/(Fraction const &frac)
 {
+    return Fraction(0, 1);
 }
 
 fractions::Fraction fractions::Fraction::simplify(Fraction frac)
 {
+    return Fraction(0, 1);
 }
 
 void fractions::Fraction::simplify()
@@ -30,12 +35,15 @@ void fractions::Fraction::simplify()
 
 int fractions::Fraction::gcd(int a, int b)
 {
+    return 0;
 }
 
 bool fractions::Fraction::operator==(Fraction const &frac)
 {
+    return false;
 }
 
 ostream &fractions::operator<<(ostream &os, const Fraction &frac)
 {
+    return os;
 }
